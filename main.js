0606eeeb618209b53588a55e6f1213800bfd8be3
EBrown2124 <110208836+EBrown2124@users.noreply.github.com>
2023-11-20 01:35:51 -0700

    finished

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 document.querySelectorAll(".question").forEach((question) => question.addEventListener("click", () => {
   if(question.parentNode.classList.contains("active")) {
-    
+
     question.parentNode.classList.toggle("active")
 
   }else{
-    document.querySelectorAll(".question").forEach(question => question.parentNode.classList.remove("active"))
+    document.querySelectorAll(".question")
+    .forEach((question) => question.parentNode.classList.remove("active"))
 
     question.parentNode.classList.add("active")
-
   }
-
 }))
 
 const info = document.getElementsByClassName('contentBox');
